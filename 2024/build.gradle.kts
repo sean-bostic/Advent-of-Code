8102b297b27f10e3bc1d6e38c9bf3bfc01b65213
seanworkz <sean.bostic@gmail.com>
2025-09-29 16:25:10 -0400

    Trying to get compose to work in this project

--- 2024/build.gradle.kts
+++ 2024/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     kotlin("jvm") version "2.2.0"
+    id("org.jetbrains.compose") version "1.8.0"
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.0"
 }
 
 group = "sean"
@@ -7,9 +9,15 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    google()
 }
 
 dependencies {
+    // Compose Desktop
+    implementation(compose.desktop.currentOs)
+    implementation(compose.material3)
+
     testImplementation(kotlin("test"))
 }
 
@@ -19,3 +27,4 @@ tasks.test {
 kotlin {
     jvmToolchain(21)
 }
+
