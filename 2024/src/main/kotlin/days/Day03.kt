e810b37abf97aefc03d91c7a14a3bf1271af01ab
seanworkz <sean.bostic@gmail.com>
2025-10-01 10:11:41 -0400

    Day 3 part 2

--- 2024/src/main/kotlin/days/Day03.kt
+++ 2024/src/main/kotlin/days/Day03.kt
@@ -9,10 +9,39 @@ class Day03 : Day(3){
             .sum()
     }
 
-    override fun part2(input: List<String>): Any {
-        return 0 // finish in the top of the morning
+    override fun part2(input: List<String>): Int =
+        input.joinToString("")
+            .findEnabledMulInstructions()
+            .sum()
+}
+
+private fun String.findAllMulInstructions(): Sequence<Int> =
+    indices.asSequence()
+        .mapNotNull { parseMulAt(it) }
+
+private fun String.findEnabledMulInstructions(): Sequence<Int> = sequence {
+    var enabled = true
+
+    for (i in indices) {
+        when {
+            startsWith("do()", i) -> enabled = true
+            startsWith("don't()", i) -> enabled = false
+            enabled -> parseMulAt(i)?.let { yield(it) }
+        }
     }
 }
+
+private fun String.parseMulAt(index: Int): Int? =
+    drop(index)
+        .takeIf { it.startsWith("mul(") }
+        ?.drop(4)
+        ?.takeWhile { it != ')' }
+        ?.split(',')
+        ?.takeIf { it.size == 2 }
+        ?.let { (first, second) ->
+            first.toIntOrNull()?.times(second.toIntOrNull() ?: return null)
+        }
+
 
 private fun Int.parseMulAt(text: String): Int? =
     text.drop(this)
--- 2024/src/test/kotlin/Day03Test.kt
+++ 2024/src/test/kotlin/Day03Test.kt
@@ -1,5 +1,6 @@
 import days.Day03
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class Day03Test {
     private val day = Day03()
@@ -9,5 +10,19 @@ class Day03Test {
         val testInput = day.loadTestInput()
         val result = day.part1(testInput)
         assert(result == 161)
+    }
+
+    @Test
+    fun `Day 3 Part 2`() {
+        // different test input so hardcode the test input as a string
+        val testInput = listOf(
+            """
+        xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+
+        mul(32,64](mul(11,8)undo()?mul(8,5))
+        """.trimIndent()
+        )
+
+        val result = day.part2(testInput)
+        assertEquals(48, result)
     }
 }
