935949fcd11537ac95eb76ab0b4d73c6486568e0
seanworkz <sean.bostic@gmail.com>
2025-10-04 21:32:28 -0400

    Day 6 Solutions

--- 2024/src/main/kotlin/days/DayRegistry.kt
+++ 2024/src/main/kotlin/days/DayRegistry.kt
@@ -6,7 +6,8 @@ object DayRegistry {
         2 to Day02(),
         3 to Day03(),
         4 to Day04(),
-        5 to Day05()
+        5 to Day05(),
+        6 to Day06(),
     )
 
     fun getDay(dayNumber: Int): Day? = days[dayNumber]
